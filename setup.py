086ca51053e418120c42f39aa7d66110ffa7e925
Allis Tauri <allista@gmail.com>
2018-01-30 13:09:39 +0300

    Added git-replace-remote-all to bulk-change remote urls

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 
 setup(
         name='gitlab-clone-all',
-        version='1.0',
+        version='1.1',
         packages=[''],
         url='',
         license='MIT',
         author='Evgeny Taranov',
         author_email='evgeny.taranov@gmail.com',
-        description='Clone all repositories from a gitlab server available to user',
-        scripts=['gitlab-clone-all', 'git-fetch-all', 'git-set-author-all', 'git-checkout-all'],
+        description='Tool to work with multi-repo projects '
+                    'based on git and gitlab',
+        scripts=['gitlab-clone-all',
+                 'git-fetch-all',
+                 'git-set-author-all',
+                 'git-checkout-all',
+                 'git-replace-remote-all']
 )
