b7184cfff59e9f0c05461fee933d9c2da229a2e3
Evgeny Taranov <taranov@jet.su>
2020-05-28 19:48:10 +0300

    Annotated types in tip_handler

--- gitlab_clone_all_utils/tip_handler.py
+++ gitlab_clone_all_utils/tip_handler.py
@@ -1,11 +1,19 @@
+from typing import Iterator, Set, Dict, Union
+
+from git import Commit, Head, Blob, Tree, TagObject
+
+CommitSet = Set[Union[TagObject, Tree, Commit, Blob]]
+HeadToCommitMap = Dict[Head, CommitSet]
+
+
 class TipHandler(object):
     @staticmethod
-    def _commits_per_head(heads):
-        return {head: set([head.commit] + list(head.commit.iter_parents()))
+    def _commits_per_head(heads: Iterator[Head]) -> HeadToCommitMap:
+        return {head: {head.commit} | set(head.commit.iter_parents())
                 for head in heads}
 
     @staticmethod
-    def _is_tip(head, commits):
+    def _is_tip(head: Head, commits: HeadToCommitMap) -> bool:
         tip = True
         for other in commits:
             if head.commit != other.commit:
