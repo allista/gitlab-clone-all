55f38790389f3186760ce4d7d149d298140820a6
Evgeny Taranov <taranov@jet.su>
2020-05-28 19:25:14 +0300

    RepoProcessor walks the tree recursively, ignoring .git dirs

--- gitlab_clone_all_utils/repo_processor.py
+++ gitlab_clone_all_utils/repo_processor.py
@@ -10,24 +10,28 @@ class RepoProcessor(object):
         self._args = args
         self._path = path
 
-    def run(self):
-        os.chdir(self._path)
-        root = os.getcwd()
-        for entry in sorted(os.listdir('.')):
-            path = os.path.join(root, entry)
-            if os.path.isdir(path):
-                os.chdir(path)
-                try:
-                    try:
-                        repo = Repo(path)
-                    except InvalidGitRepositoryError:
-                        continue
-                    if repo.bare:
-                        continue
-                    self._process(repo, path)
-                except Exception as ex:
-                    print('Error while processing {}:\n{!s}\n{}'
-                          .format(path, ex, traceback.format_exc()))
+    def _process_path(self, path: str) -> None:
+        try:
+            os.chdir(path)
+            try:
+                repo = Repo(path)
+            except InvalidGitRepositoryError:
+                return
+            if repo.bare:
+                return
+            self._process(repo, path)
+        except Exception as ex:
+            print('Error while processing {}:\n{!s}\n{}'
+                  .format(path, ex, traceback.format_exc()))
+
+    def run(self) -> int:
+        start_path = os.path.abspath(self._path)
+        self._process_path(start_path)
+        for root, dirs, _files in os.walk(start_path):
+            for dirname in dirs:
+                if dirname == '.git':
+                    continue
+                self._process_path(os.path.join(root, dirname))
         return 0
 
     def _process(self, repo, path):
